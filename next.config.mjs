cbf725303f263b0c8209343fbc9736f276979207
Alban Caré <alban.care@gmail.com>
2024-10-02 10:03:18 +0000

    chore: Try to resolve "Error: TypeError: Error must be an Error instance" when deploying github page

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,16 @@
-const createMDX = require("@next/mdx");
+import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-const withMDX = createMDX({});
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
 
 export default withMDX(nextConfig);
